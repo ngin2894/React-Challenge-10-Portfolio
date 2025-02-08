1def0b920c0a634e74efe72da4c618bb031d6b2f
ngin2894 <nataliegindraux@gmail.com>
2025-02-08 14:00:37 -0600

    added all remaining github links & images for different portfolios to meet acceptance criteria

--- my react portfolio/src/components/Portfolio.jsx
+++ my react portfolio/src/components/Portfolio.jsx
@@ -2,10 +2,10 @@ function Portfolio() {
     const projects = [
       { title: "SQL Employee Database", image: "/src/assets/SQLDB.png", github: "https://github.com/ngin2894/SQL-Challenge-10-CMS" },
       { title: "Foodie", image: "/src/assets/logo2.jpg", github: "https://github.com/musserdn/Foodie" },
-      { title: "Weather Dashboard", image: "/src/assets/weatherdashboard.png", github: ""},
-      { title: "Typescript Vehicles", image: "/src/assets/workdayscheduler.png", github: ""},
-      { title: "ReadMe Generator", image: "/src/assets/reactportfolio.png", github: ""},
-      { title: "React Portfolio", image: "/src/assets/reactportfolio.png", github: ""}
+      { title: "Weather Dashboard", image: "/src/assets/weatherdashboard.png", github: "https://github.com/ngin2894/weather-challenge-9"},
+      { title: "Typescript Vehicles", image: "/src/assets/vehicles.png", github: "https://github.com/ngin2894/Challenge-8-Typescript-Vehicles"},
+      { title: "ReadMe Generator", image: "/src/assets/ReadMe.png", github: "https://github.com/ngin2894/ReadMe-Generator"},
+      { title: "React Portfolio", image: "/src/assets/ReactPortfolio.png", github: "https://github.com/ngin2894/React-Challenge-10-Portfolio"}
     ];
   
     return (
