96e58b87c2329726e7ad5431509f932fd039b7e8
ngin2894 <nataliegindraux@gmail.com>
2025-02-08 12:55:27 -0600

    Added images & links to the Portfolio Tab

--- my react portfolio/src/components/Portfolio.jsx
+++ my react portfolio/src/components/Portfolio.jsx
@@ -1,7 +1,7 @@
 function Portfolio() {
     const projects = [
-      { title: "Project 1", image: "path-to-image.jpg", link: "#", github: "#" },
-      { title: "Project 2", image: "path-to-image.jpg", link: "#", github: "#" },
+      { title: "SQL Employee Database", image: "/src/assets/SQLDB.png", github: "https://github.com/ngin2894/SQL-Challenge-10-CMS" },
+      { title: "Foodie", image: "/src/assets/logo2.jpg", github: "https://github.com/musserdn/Foodie" },
     ];
   
     return (
@@ -11,10 +11,10 @@ function Portfolio() {
           {projects.map((project, index) => (
             <div key={index}>
               <h3>{project.title}</h3>
-              <img src={project.image} alt={project.title} />
-              <p>
-                <a href={project.link}>Live Demo</a> | <a href={project.github}>GitHub</a>
-              </p>
+              <img className="portfolio-image" src={project.image} alt={project.title} />
+              <h3>
+                <a href={project.github}>GitHub</a>
+              </h3>
             </div>
           ))}
         </div>
